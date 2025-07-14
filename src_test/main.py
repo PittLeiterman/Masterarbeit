13c2060873f1c005d9a4da560b53a9a7b647c3d1
Pitt Leitermann <pitt@solidpulse.dev>
2025-07-14 10:29:50 +0200

    added new shapes

--- src_test/main.py
+++ src_test/main.py
@@ -1,9 +1,9 @@
 import json
 from execution.runner import run_admm_trajectory_optimization
 
-CONFIG = "shape1"
+CONFIG = "shape3"
 
 with open(f"input/configs/{CONFIG}.json", "r") as f:
     config = json.load(f)
 
-run_admm_trajectory_optimization(config, DEBUG=True)
+run_admm_trajectory_optimization(config, DEBUG=False)
